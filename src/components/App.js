fcfc3d9796b290c02367365590663c71c031cd7d
TawshifTamjidAlam <tawshif27@gmail.com>
2019-06-06 18:49:46 -0700

    Add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import '../index.css';
+import React from "react";
+import Header from "./Header";
+import "../index.css";
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
     </div>
   );
 }
