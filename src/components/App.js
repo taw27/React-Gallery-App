ed55fbbd3bb686e95c800aa385bafd720945fa14
TawshifTamjidAlam <tawshif27@gmail.com>
2019-06-08 16:38:21 -0700

    add BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "./Header";
 import "../index.css";
+import {BrowserRouter} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header></Header>
+      </div>
+    </BrowserRouter>
   );
 }
 
