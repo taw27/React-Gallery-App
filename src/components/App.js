f2ae9a65769ed08450625d48eeb13133b08df941
TawshifTamjidAlam <tawshif27@gmail.com>
2019-06-08 16:45:35 -0700

    Change in to class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, {Component} from "react";
 import Header from "./Header";
 import "../index.css";
 import {BrowserRouter} from "react-router-dom"
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Header></Header>
-      </div>
-    </BrowserRouter>
-  );
+class App extends Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Header></Header>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
