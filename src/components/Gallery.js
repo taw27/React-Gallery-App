39b26b1f19a7a50d310c678334a9655475a7acc5
TawshifTamjidAlam <tawshif27@gmail.com>
2019-06-08 19:23:34 -0700

    Add Gallery Component

---
+++ src/components/Gallery.js
@@ -0,0 +1,26 @@
+import React from "react-dom";
+import Proptypes from "prop-types";
+import GalleryItem from "./GalleryItem";
+import NoResults from "./NoResults";
+
+const Gallery = ({title, photos}) =>{
+    const isEmpty = ! (photos.length > 0);
+    return(
+        <div className="photo-container">
+            <h2> {isEmpty ? "" : title } </h2>
+            {
+                !isEmpty ?
+                photos.map((photo) => 
+                    <GalleryItem farm={photo.farm} key={photo.id} server={photo.server} secret={photo.secret} ></GalleryItem>
+                ) : <NoResults></NoResults>
+            }
+        </div>
+    );
+};
+
+Gallery.propTypes = {
+    title: Proptypes.string,
+    photos: Proptypes.array
+}
+
+export default Gallery;
