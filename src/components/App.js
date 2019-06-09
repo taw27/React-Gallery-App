012e28d7453294eed1ed174a598d565cb355b1ed
TawshifTamjidAlam <tawshif27@gmail.com>
2019-06-09 01:20:04 -0700

    Add Gallery & methods to display initial cat route

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React, {Component} from "react";
 import Header from "./Header";
 import "../index.css";
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter, Route, Redirect} from "react-router-dom"
+import Gallery from "./Gallery";
+import axios from "axios";
+import apiKey from '../config';
 
 class App extends Component {
   constructor(){
     super();
+
+    this.state = {
+      searchData: [],
+      catsData: [],
+      dogsData: [],
+      birdsData: []
+    }
+  }
+
+  componentDidMount(){
+    this.fetchPhotos("cat").then((photos) => this.setState({catsData: photos}));
+  }
+
+  async fetchPhotos(searchTag){
+    const response = await axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${searchTag}&format=json&nojsoncallback=1&per_page=24`);
+    const {data} =  await response;
+    return data.photos.photo;
   }
 
   render(){
@@ -13,6 +33,8 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Header></Header>
+          <Route exact path="/" render={() => (<Redirect to="/cats"></Redirect>)}></Route>
+          <Route path="/cats" render={() => (<Gallery title="Cats" photos={this.state.catsData}></Gallery>)}></Route>
         </div>
       </BrowserRouter>
     );
