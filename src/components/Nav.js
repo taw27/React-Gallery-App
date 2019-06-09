8464ad5f5867a3f2049de5b8ea2e9d9b5daa2727
TawshifTamjidAlam <tawshif27@gmail.com>
2019-06-09 01:15:16 -0700

    Fix NavLink routes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,8 +4,8 @@ import {NavLink} from "react-router-dom";
 const Nav = () => (
     <nav className="main-nav">
         <ul>
-            <li> <NavLink to="/cat"> Cats </NavLink> </li>
-            <li> <NavLink to="/dog"> Dogs </NavLink> </li>
+            <li> <NavLink to="/cats"> Cats </NavLink> </li>
+            <li> <NavLink to="/dogs"> Dogs </NavLink> </li>
             <li> <NavLink to="/birds"> Birds </NavLink> </li>
         </ul>
     </nav>
