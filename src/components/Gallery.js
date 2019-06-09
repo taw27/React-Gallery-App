44be0c3dc715a692a9ee3586f1172ac82e8da2ce
TawshifTamjidAlam <tawshif27@gmail.com>
2019-06-09 01:16:40 -0700

    add id prop

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -14,6 +14,7 @@ const Gallery = ({ title, photos }) => {
             <GalleryItem
                 farm={photo.farm}
                 key={photo.id}
+                id={photo.id}
                 server={photo.server}
                 secret={photo.secret}
             />
