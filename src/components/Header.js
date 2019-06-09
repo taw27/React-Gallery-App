b1def7d954769f662f421fd99e6d563a27a89bc5
TawshifTamjidAlam <tawshif27@gmail.com>
2019-06-09 02:06:17 -0700

    Add Search component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Proptypes from "prop-types";
 import Nav from "./Nav";
+import Search from "./Search";
 
-const Header = props => (
+const Header = ({title, handleSearch}) => (
   <div>
-    <h1>{props.title}</h1>
+    <h1>{title}</h1>
+    <Search handleSearch={handleSearch}></Search>
     <Nav />
   </div>
 );
@@ -14,6 +16,7 @@ Header.defaultProps = {
 };
 
 Header.propTypes = {
-  title: Proptypes.string
+  title: Proptypes.string,
+  handleSearch: Proptypes.func
 };
 export default Header;
