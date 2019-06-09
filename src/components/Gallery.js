798bee5148828024129fd537c47ed69199cef9b0
TawshifTamjidAlam <tawshif27@gmail.com>
2019-06-08 19:30:57 -0700

    format with prettier

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,24 +3,30 @@ import Proptypes from "prop-types";
 import GalleryItem from "./GalleryItem";
 import NoResults from "./NoResults";
 
-const Gallery = ({title, photos}) =>{
-    const isEmpty = ! (photos.length > 0);
-    return(
-        <div className="photo-container">
-            <h2> {isEmpty ? "" : title } </h2>
-            {
-                !isEmpty ?
-                photos.map((photo) => 
-                    <GalleryItem farm={photo.farm} key={photo.id} server={photo.server} secret={photo.secret} ></GalleryItem>
-                ) : <NoResults></NoResults>
-            }
-        </div>
-    );
+const Gallery = ({ title, photos }) => {
+  const isEmpty = !(photos.length > 0);
+  return (
+    <div className="photo-container">
+      <h2> {isEmpty ? "" : title} </h2>
+      {!isEmpty ? (
+        photos.map(photo => (
+          <GalleryItem
+            farm={photo.farm}
+            key={photo.id}
+            server={photo.server}
+            secret={photo.secret}
+          />
+        ))
+      ) : (
+        <NoResults />
+      )}
+    </div>
+  );
 };
 
 Gallery.propTypes = {
-    title: Proptypes.string,
-    photos: Proptypes.array
-}
+  title: Proptypes.string,
+  photos: Proptypes.array
+};
 
 export default Gallery;
