035da07c59428e188299da471540528e3038da18
TawshifTamjidAlam <tawshif27@gmail.com>
2019-06-10 04:12:07 -0700

    Change to stateless functional component

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,37 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 import Proptypes from "prop-types";
 import GalleryItem from "./GalleryItem";
 import MessageLi from "./MessageLi";
 
-class Gallery extends Component {
-  render() {
-    const { photos, title } = this.props;
-    const isEmpty = !(photos.length > 0);
-    const errorMessage = "You search did not return any results. Please try a different query" ;
-    const errorTitle= "No Results Found";
-    return (
-      <div className="photo-container">
-        <h2> {isEmpty ? "" : title} </h2>
-        <ul>
-          {!isEmpty ? (
-            photos.map(photo => (
-              <GalleryItem
-                farm={photo.farm}
-                key={photo.id}
-                id={photo.id}
-                server={photo.server}
-                secret={photo.secret}
-                title={photo.title}
-              />
-            ))
-          ) : (
-            <MessageLi messageTitle={errorTitle} messageText={errorMessage}/>
-          )}
-        </ul>
-      </div>
-    );
-  }
-}
+const Gallery = ({ photos, title }) => {
+  const isEmpty = !(photos.length > 0);
+  const errorMessage =
+    "You search did not return any results. Please try a different query";
+  const errorTitle = "No Results Found";
+  return (
+    <div className="photo-container">
+      <h2> {isEmpty ? "" : title} </h2>
+      <ul>
+        {!isEmpty ? (
+          photos.map(photo => (
+            <GalleryItem
+              farm={photo.farm}
+              key={photo.id}
+              id={photo.id}
+              server={photo.server}
+              secret={photo.secret}
+              title={photo.title}
+            />
+          ))
+        ) : (
+          <MessageLi messageTitle={errorTitle} messageText={errorMessage} />
+        )}
+      </ul>
+    </div>
+  );
+};
 
 Gallery.propTypes = {
   title: Proptypes.string,
