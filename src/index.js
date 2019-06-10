b1ac4e81af71c70943f709a8a8676ff33eed3175
TawshifTamjidAlam <tawshif27@gmail.com>
2019-06-10 02:56:49 -0700

    Add Browser ROuter and Route for App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  <BrowserRouter>
+  <Route path="/" component={App} />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
