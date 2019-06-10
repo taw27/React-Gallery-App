b71d07c7f791fcec5b4d2cd1abbebb426097f3bd
TawshifTamjidAlam <tawshif27@gmail.com>
2019-06-09 18:26:07 -0700

    Fetch query string on componentMount

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,36 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import Proptypes from "prop-types";
 import GalleryItem from "./GalleryItem";
 import NoResults from "./NoResults";
+import queryString from "query-string";
 
-const Gallery = ({ title, photos }) => {
-  const isEmpty = !(photos.length > 0);
-  return (
-    <div className="photo-container">
-      <h2> {isEmpty ? "" : title} </h2>
-      <ul>
-        {!isEmpty ? (
-          photos.map(photo => (
-            <GalleryItem
-              farm={photo.farm}
-              key={photo.id}
-              id={photo.id}
-              server={photo.server}
-              secret={photo.secret}
-              title={photo.title}
-            />
-          ))
-        ) : (
-          <NoResults />
-        )}
-      </ul>
-    </div>
-  );
-};
+class Gallery extends Component {
+  componentDidMount() {
+    const { location, fetchSearchImages } = this.props;
+
+    if (location.search) {
+      const values = queryString.parse(this.props.location.search);
+      fetchSearchImages(values.query);
+    }
+  }
+
+  render() {
+    const { photos, title } = this.props;
+    const isEmpty = !(photos.length > 0);
+    return (
+      <div className="photo-container">
+        <h2> {isEmpty ? "" : title} </h2>
+        <ul>
+          {!isEmpty ? (
+            photos.map(photo => (
+              <GalleryItem
+                farm={photo.farm}
+                key={photo.id}
+                id={photo.id}
+                server={photo.server}
+                secret={photo.secret}
+                title={photo.title}
+              />
+            ))
+          ) : (
+            <NoResults />
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
 
 Gallery.propTypes = {
   title: Proptypes.string,
-  photos: Proptypes.array
+  photos: Proptypes.array,
+  location: Proptypes.object,
+  fetchSearchImages: Proptypes.func
 };
 
 export default Gallery;
